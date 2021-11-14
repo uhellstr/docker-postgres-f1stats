8da8f58e57ccca993fa9ed0d8a0e3b89a8fce5dd
Ulf Hellström <oraminute@gmail.com>
2021-11-14 11:12:35 +0100

    Translated some more queries from Oracle to Postgres

--- demo/demo_queries.sql
+++ demo/demo_queries.sql
@@ -31,7 +31,6 @@ where vfr.season = date_part('year', now())::smallint
                                 select coalesce(min(race)-1,-1) as race -- check if any upcoming races this seaseon -1 and season is done
                                 from v_f1_upcoming_races
                                 where season = date_part('year', now())::smallint
-                                  --and to_date(race_date,'RRRR-MM-DD') <= trunc(sysdate)
                               )
                               select case when race = -1 then (select max(race)
                                                                from  v_f1_racedates
@@ -44,7 +43,6 @@ order by vfr.driverposition asc
 fetch first 10 rows only;
 
 -- Get us the top number pole positions for the current season
--- NOT YET TRANSLATED COMPLETLY TO POSTGRESQL
 with polesitters as (select familyname
       ,count(familyname) as pole_positions
 from v_f1_qualificationtimes
@@ -58,33 +56,117 @@ select familyname
  
 -- Give us all world champions in Formula 1!!
 select
-    d.season,
-    d.givenname,
-    d.familyname,
-    d.nationality,    
-    d.points,
-    d.wins,
-    d.info,
-    d.constructorname,
-    d.constructorinfo,
-    d.constructornationality
+    d.season
+    ,d.givenname
+    ,d.familyname
+    ,d.nationality    
+    ,d.points
+    ,d.wins
+    ,d.info
+    ,d.constructorname
+    ,d.constructorinfo
+    ,d.constructornationality
 from v_f1_driverstandings d
 where d.race = (select max(e.race)
                 from v_f1_driverstandings e
-                 where e.season = d.season)
+                where e.season = d.season)
       and d.driverposition = 1
-      and d.season <= (select season -- Is current season finished yet?
+      and d.season <= (select x.season -- Is current season finished yet?
                        from
                        (
-                         select to_date(r.race_date,'RRRR-MM-DD') as race_date
+                         select to_date(r.racedate::varchar,'YYYY-MM-DD') as racedate
                                 ,case 
-                                   when r.race_date < trunc(sysdate) then to_char(trunc(sysdate),'RRRR')
-                                   when r.race_date > trunc(sysdate) then to_char(to_number(to_char(trunc(sysdate),'RRRR'))-1)
-                                   else '1900'
+                                   when to_date(r.racedate::varchar,'YYYY-MM-DD') < now()::date then date_part('year', now())::smallint
+                                   when to_date(r.racedate::varchar,'YYYY-MM-DD') > now()::date then date_part('year', now())::smallint -1
+                                   else 1900::smallint
                                  end as season
-                         from f1_access.v_f1_seasons_race_dates r
+                         from v_f1_racedates r
                          where r.season = d.season
-                           and to_number(r.round) in (select max(to_number(rd.round)) from f1_access.v_f1_seasons_race_dates rd
-                                                      where rd.season  = r.season)
-                        ))
+                           and r.race in (select max(rd.race) 
+													                from v_f1_racedates rd
+                                          where rd.season  = r.season)
+                        ) x )
 order by d.season desc;
+
+-- Give us the number of championships a champ has got! E.g who is the ultimate champ!
+select y.givenname,
+       y.familyname,
+       y.nationality,
+       y.championships_won
+from
+(select x.driverid,
+       x.givenname,
+       x.familyname,
+       x.nationality,
+       count(x.driverid) as championships_won
+from 
+(select
+    d.season,
+    d.race,
+    d.driverposition,
+    d.positiontext,
+    d.points,
+    d.wins,
+    d.driverid,
+    d.info,
+    d.givenname,
+    d.familyname,
+    d.dateofbirth,
+    d.nationality,
+    d.constructorid,
+    d.constructorinfo,
+    d.constructorname,
+    d.nationality as contructornationality
+from
+    v_f1_driverstandings d
+    where d.race = (select max(e.race)
+                    from v_f1_driverstandings e
+                    where e.season = d.season)
+      and d.driverposition = 1
+      and d.season <= (select x.season -- Is current season finished yet?
+                       from
+                       (
+                         select to_date(r.racedate::varchar,'YYYY-MM-DD') as racedate
+                                ,case 
+                                   when to_date(r.racedate::varchar,'YYYY-MM-DD') < now()::date then date_part('year', now())::smallint
+                                   when to_date(r.racedate::varchar,'YYYY-MM-DD') > now()::date then date_part('year', now())::smallint -1
+                                   else 1900::smallint
+                                 end as season
+                         from v_f1_racedates r
+                         where r.season = d.season
+                           and r.race in (select max(rd.race) 
+													                from v_f1_racedates rd
+                                          where rd.season  = r.season)
+                        ) x )
+) x group by driverid,givenname,familyname,nationality
+) y order by championships_won desc;
+
+-- Give us the constructor champions over the years
+select
+  c.season,
+  c.points,
+  c.wins,
+  c.constructorname,
+  c.info as constructorinfo,
+  c.nationality as contructornationality
+from v_f1_constructorstandings c
+where c.race = (select max(d.race)
+                from v_f1_racedates d
+                where d.season = c.season)
+  and c.constructorposition = '1'
+  and c.season <=  (select x.season -- Is current season finished yet?
+                       from
+                       (
+                         select to_date(r.racedate::varchar,'YYYY-MM-DD') as racedate
+                                ,case 
+                                   when to_date(r.racedate::varchar,'YYYY-MM-DD') < now()::date then date_part('year', now())::smallint
+                                   when to_date(r.racedate::varchar,'YYYY-MM-DD') > now()::date then date_part('year', now())::smallint -1
+                                   else 1900::smallint
+                                 end as season
+                         from v_f1_racedates r
+                         where r.season = c.season
+                           and r.race in (select max(rd.race) 
+													                from v_f1_racedates rd
+                                          where rd.season  = r.season)
+                        ) x )
+order by c.season desc;
